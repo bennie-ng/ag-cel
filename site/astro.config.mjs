89533fd019d8bc773e5c6a51da1637a179fe175d
Ben Ng <hoangna1204.dev@gmail.com>
2026-02-13 14:13:23 +0700

    fix: Update base path and internal links from `/agcel` to `/AgCel`.

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -5,7 +5,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://bennie-ng.github.io',
-	base: '/agcel',
+	base: '/AgCel',
 	integrations: [
 		starlight({
 			title: 'AgCel',
